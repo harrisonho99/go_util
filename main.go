9c3f9eb0588a657bcf26c0c930fb4c37f31fca19
hotsnow199 <socrom001@gmail.com>
2022-03-30 15:11:13 +0700

    use watch file

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	// useWatchFile()
-	ch := make(chan int)
-	go func() {
-		close(ch)
-	}()
-	for range ch {
-	}
-	var sl = []int{1, 2, 3, 4}
-	fmt.Println(sl[1:])
+	useWatchFile()
 }
